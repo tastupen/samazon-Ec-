a337c3703a332da73f473e3baaa324e3424278a6
tastupen <tatsupen0828@au.com>
2021-11-14 16:49:45 +0000

    15 users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,13 +5,14 @@ class ProductsController < ApplicationController
   def index
     if sort_params.present?
       @products = Product.sort_products(sort_params, params[:page])
-      @category = Category.request_category(category_params)
+      @category = Category.request_category(sort_params[:sort_category])
     elsif params[:category].present?
       @category = Category.request_category(params[:category])
       @products = Product.category_products(@category, params[:page])
     else
       @products = Product.display_list(params[:page])
     end
+    
     @categories = Category.all
     @major_category_names = Category.major_categories
     @sort_list = Product.sort_list
